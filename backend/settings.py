0e221ba9a0bc41f983b9aafbc04b65033335b159
nhatbarry <nhatbarry@gmail.com>
2025-06-10 03:56:42 +0700

    minor

--- backend/settings.py
+++ backend/settings.py
@@ -113,9 +113,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'frontend/build')
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
