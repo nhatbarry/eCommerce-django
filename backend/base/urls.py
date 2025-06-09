195149bf8a25db3b2ef9d00dc051bd5f80a22962
nhatbarry <nhatbarry@gmail.com>
2025-06-10 00:52:35 +0700

    be

--- backend/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.getRoutes, name='routes'),
-    path('prods/', views.getProds, name='prods'),
-    path('prod/<str:pk>/', views.getProd, name='prod'),
-]
