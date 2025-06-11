a1bbc3bb01e7fa7a37ac35ca2668ef1d7b5fb786
nhatbarry <nhatbarry@gmail.com>
2025-06-11 21:40:32 +0700

    upload to azure

--- backend/settings.py
+++ backend/settings.py
@@ -60,13 +60,27 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
 ]
 
-DEFAULT_FILE_STORAGE = 'base.storage_backends.AzureMediaStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.azure_storage.AzureStorage",
+        "OPTIONS": {
+            "account_name": env("AZURE_ACCOUNT_NAME"),
+            "account_key": env("AZURE_ACCOUNT_KEY"),
+            "azure_container": env("AZURE_CONTAINER"),
+            "azure_ssl": True,
+            "expiration_secs": None,
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
 
 AZURE_ACCOUNT_NAME = env('AZURE_ACCOUNT_NAME')
-AZURE_ACCOUNT_KEY = env('AZURE_ACCOUNT_KEY')
 AZURE_CONTAINER = env('AZURE_CONTAINER')
-AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net/{AZURE_CONTAINER}/'
-MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/'
+AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/'
 AZURE_SSL = True
 
 
--- base/storage_backends.py
+++
@@ -1,8 +0,0 @@
-from storages.backends.azure_storage import AzureStorage
-import os
-
-class AzureMediaStorage(AzureStorage):
-    account_name = os.environ.get('AZURE_ACCOUNT_NAME')
-    account_key = os.environ.get('AZURE_ACCOUNT_KEY')
-    azure_container = os.environ.get('AZURE_CONTAINER')
-    expiration_secs = None
